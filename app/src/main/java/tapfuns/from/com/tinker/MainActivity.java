87394c50014ff033b9bb3120486ced0f552b0d9e
mojinsheng <2880506909@qq.com>
2017-11-16 16:14:17 +0800

    Initial commit

---
+++ app/src/main/java/tapfuns/from/com/tinker/MainActivity.java
@@ -0,0 +1,16 @@
+package tapfuns.from.com.tinker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"这是一个新包",Toast.LENGTH_LONG).show();
+
+    }
+}
